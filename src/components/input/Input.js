9cee38a7be398aef6560a3d0fe46ee669dfc534c
Jeferson Ribeiro <jeffribeiro0110@gmail.com>
2021-12-28 08:31:18 -0300

    library v1.6

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -4,29 +4,26 @@ import './Input.css';
 class Input extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      ivalue:'',
-      term:''
-    };
+    this.state = {term:''};
+    this.handleInput = this.handleInput.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleInput(event){
-    const {callback} = this.props;
-    this.setState({ivalue:event.target.value});
-    callback(event.target.value);
+    this.setState({term:event.target.value});
   }
 
-  handleClick(event){
-    this.setState({term:event.target.value});
+  handleClick(){
+    const {callback} = this.props;
+    callback(this.state.next);
   }
 
   render(){
     const {mglass} = this.props;
     return(
       <div class="Input">
-        <input type='text' onChange={this.handleInput}></input>
-        <button class="button" onClick={this.handleClick} value={this.ivalue}>{mglass}</button>
+        <input type="text" onChange={this.handleInput}/>
+        <button class="button" onClick={this.handleClick}>{mglass}</button>
       </div>
     );
 	}
