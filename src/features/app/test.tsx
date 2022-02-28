9870780ea844cf230441836fb5453cf613304f12
Jeferson Ribeiro <jeffribeiro0110@gmail.com>
2022-02-28 17:04:27 -0300

    atividade 1 do módulo 3 do curso DFE

---
+++ src/features/app/test.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import { App } from "./App";
+
+describe("App", function() {
+  it("Deve renderizar o título da aplicação corretamente", function() {
+    expect(true).toBe(true)
+  });
+});
